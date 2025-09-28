b55e94b3080ea63046aa9d07ee4450964cbace7d
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-09-28 17:38:49 +0900

    feat: test code for kotlin

---
+++ msa/micro-service/auth/src/test/kotlin/com/msa/microserviceauth/MicroserviceAuthApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.msa.microserviceauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroserviceAuthApplicationTests {
+    @Test
+    fun contextLoads() {
+        
+    }
+}
